af45d732066bf2c80b6476186797e79cc784d7eb
Vipul Somani <vipulsomani12@gmail.com>
2020-10-26 17:05:43 +0530

    Boot

--- src/main/java/com/somani/springboot/demo/mycoolapp/MycoolappApplication.java
+++ src/main/java/com/somani/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -2,12 +2,10 @@ package com.somani.springboot.demo.mycoolapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class MycoolappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MycoolappApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/somani/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,16 @@
+package com.somani.springboot.demo.mycoolapp.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+	
+	@GetMapping("/")
+	public String sayHello() {
+		return "Time on server is " + LocalDateTime.now();
+	}
+	
+}
